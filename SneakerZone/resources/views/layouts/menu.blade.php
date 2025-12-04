d9390d07563cc88ed9ba9c538a0774bf371fd5ea
NguyenDuy189 <nguyenducduy2004hakhe@gmail.com>
2025-12-04 20:54:58 +0700

    fix: remove nested SneakerZone folder pushed by mistake

--- SneakerZone/resources/views/layouts/menu.blade.php
+++
@@ -1,9 +0,0 @@
-<ul class="category-menu">
-    @foreach ($categories as $cate)
-        <li>
-            <a href="{{ route('category.products', $cate->slug) }}">
-                {{ $cate->name }}
-            </a>
-        </li>
-    @endforeach
-</ul>
